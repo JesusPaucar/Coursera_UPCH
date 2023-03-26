cbd325454d35254a0be62d7ee62345221be02278
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2023-03-26 11:33:05 -0300

    Create upload_pdf.py

---
+++ upload_pdf.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import pdfplumber
+import traslators as ts
+import translator.server as tss
+from cleantext import clean
+import streamlit as st
+#import re
+#import nltk
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
+#from nltk.corpus import stopwords
+#from nltk.tokenize import word_tokenize
+#from nltk.stem import WordNetLemmatizer
+#from google.colab import files
+#from sentence_transformers import SentenceTransformer, util
+
+def get_text(pdf_filename, translate = False):
+  text = ''
+  with pdfplumber.open(pdf_filename) as pdf:
+    for p in pdf.pages:
+      text += p.extract_text()
+  text = clean(text)
+  if translate:
+    n = 2000
+    text = [text[i:i+n] for i in range(0, len(text), n)]
+    text = [tss.google(t, 'es', 'en',) for t in text]
+    text = ' '.join(text)
+  return text
+
+upload_file = st.file_uploader('Choose yout .pdf file', type = 'pdf')
+if uploaded_file is not None:
+  df = extract_data(uploaded_file)
+  silabo = get_text(df, translate = True)
+
+st.write(silabo)
