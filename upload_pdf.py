bff1e9771918da71af4314302e31c27e550e6cf8
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2023-03-26 11:54:15 -0300

    Update upload_pdf.py

--- upload_pdf.py
+++ upload_pdf.py
@@ -5,15 +5,15 @@ import translators as ts
 import translators.server as tss
 from cleantext import clean
 import streamlit as st
-#import re
-#import nltk
-#nltk.download('punkt')
-#nltk.download('stopwords')
-#nltk.download('wordnet')
-#nltk.download('omw-1.4')
-#from nltk.corpus import stopwords
-#from nltk.tokenize import word_tokenize
-#from nltk.stem import WordNetLemmatizer
+import re
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 #from google.colab import files
 #from sentence_transformers import SentenceTransformer, util
 
@@ -30,8 +30,24 @@ def get_text(pdf_filename, translate = False):
     text = ' '.join(text)
   return text
 
+def data_preprocessing(review):
+  stop_words = stopwords('english')
+  lemmatizer = WordNetLemmatizer()
+  
+  review = re.sub(re.compile('<.*?>'), '', review)
+  review = re.sub('[^A-Za-z0+9]+', ' ', review)
+  
+  review = review.lower()
+  
+  tokens = nltk.word_tokenize(review)
+  review = [word for word in tokens if word not in stop_words]
+  review = [lemmatizer.lemmatize(word) for word in review]
+  review = ' '.join(review)
+  return review
+
 uploaded_file = st.file_uploader('Choose yout .pdf file', type = 'pdf')
 if uploaded_file is not None:
   #df = extract_data(uploaded_file)
   silabo = get_text(uploaded_file, translate = True)
-  st.write(silabo)
+  content = data_preprocessing(silabo)
+  st.write(content)
